ee9f13d509c49bd3a9c634c44078408e1c22c810
user <user@DESKTOP-ML6KUHU>
2023-12-24 21:41:52 +0300

    Добавьте файлы проекта.

---
+++ Course(HomeWork#4)/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_HomeWork_4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                uint oddNumbersCount = 0;
+                uint evenNumbersCount = 0;
+                int oddNumbersSum = 0;
+                int evenNumbersSum = 0;
+                int currentValue;
+                int limit;
+
+                try
+                {
+                    Console.WriteLine("Введите первое число диапазона:");
+                    currentValue = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите последнее число диапазона:");
+                    limit = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Было введено некорректное число!");
+                    Console.WriteLine("Нажмите Enter чтобы очистить консоль");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                while (currentValue <= limit)
+                {
+                    if (currentValue % 2 == 0)
+                    {
+                        evenNumbersCount++;
+                        evenNumbersSum += currentValue;
+                    }
+                    else if (currentValue % 2 != 0)
+                    {
+                        oddNumbersCount++;
+                        oddNumbersSum += currentValue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неизвестная ошибка!");
+                    }
+                    currentValue++;
+                }
+
+                Console.WriteLine($"Количество нечетных чисел: {oddNumbersCount}");
+                Console.WriteLine($"Количество четных чисел: {evenNumbersCount}");
+                Console.WriteLine($"Сумма нечетных чисел: {oddNumbersSum}");
+                Console.WriteLine($"Сумма четных чисел: {evenNumbersSum}");
+
+                Console.WriteLine("Нажмите Enter чтобы очистить консоль");
+                Console.ReadLine();
+            }
+        }
+    }
+}
